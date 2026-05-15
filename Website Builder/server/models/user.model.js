e873a26d9f0498edd229f2bffbc017c61f767177
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-05-15 10:52:33 +0545

    User Model Created

---
+++ Website Builder/server/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose =require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    avatar:{
+        type:String,
+    },
+    credits:{
+        type:Number,
+        default:100,
+        min:0
+    },
+    plan:{
+        type:String,
+        enum:['free','pro','enterprise'],
+        default:'free'
+    }
+    },{timestamps:true})
+
+    const userModel = mongoose.model('User',userSchema)
+    module.exports = userModel
+    
