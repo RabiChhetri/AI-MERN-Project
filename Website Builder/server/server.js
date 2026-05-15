9cd2f63199732d9861e70bc750ec1159b726bb91
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-05-15 10:44:15 +0545

    Connected to the database Successfully

--- Website Builder/server/server.js
+++ Website Builder/server/server.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 const express =require('express');
-
+const mongoose=require('mongoose');
+const dns=require('dns');
+dns.setServers(['8.8.8.8','8.8.4.4'])
 const app =express()
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("Connected to MongoDB")
+})
+.catch((error)=>{
+    console.error("Error connecting to MongoDB:",error)
+})
 
 app.listen(3000,()=>{
     console.log("Server is running on the port 3000")
